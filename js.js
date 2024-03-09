7ea3aa477d62d628e032adac95f73e59792cd947
kabilanero <kapilrhode0000@gmail.com>
2024-03-09 14:14:24 +0530

    commited in fronted

---
+++ js.js
@@ -0,0 +1,24 @@
+// setTimeout(()=>{
+//   console.log("hello all");
+// },3000)
+
+
+// var res={
+//     name:'kabil',
+//     age:'40',
+//     sec:"seven"
+// }
+// console.table(res);  //here the things is the code occupies not the same position so its print later at the last
+// console.log(Object.keys(res)+Object.values(res));
+
+
+// var a=[10,20,30,40]
+// a.forEach((a)=>{
+//     console.log(a);
+// })
+// console.log(a);
+
+function fn(){
+    var op=document.getElementsById("in").value;
+    console.log(op);
+};
